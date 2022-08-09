649d9186d01955045e9a063b3e25e04a5598d234
Michael Fisher <fisherdesign3@gmail.com>
2022-08-09 15:42:35 -0600

    Succesfully updating the database now.

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,37 @@
 //Accessed Via /notes
-
 const { Router, application } = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const router = Router();
 
 router.get('/', (req, res) => {
-	res.send('Notes Page will display here');
+	res.send('This is where the notes page will display');
 });
 
 router.post('/', (req, res) => {
-	console.log(req.body);
-	res.send('Posted Notes will display here');
+	const { title, text } = req.body;
+	fs.readFile('./db/db.json', 'utf8', (err, data) => {
+		if (err) {
+			console.log(err);
+			res.status(400).send('Error with /notes post');
+		} else {
+			const currentData = JSON.parse(data);
+			currentData.push({ title, text });
+			fs.writeFile(
+				'./db/db.json',
+				JSON.stringify(currentData),
+				'utf-8',
+				(err) => {
+					if (err) {
+						console.log(err);
+						res.status(400).send('There was an error with the /note write.');
+					}
+				}
+			);
+		}
+	});
+	res.send('Notes succesfully updated');
 });
 
 module.exports = router;
