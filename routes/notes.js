75e7f8df78f8da54ed27034c28d43569ba9c91cc
Michael Fisher <fisherdesign3@gmail.com>
2022-08-09 22:33:31 -0600

    Everything looks to be working. Cleaned up the code and added notes

--- routes/notes.js
+++ routes/notes.js
@@ -6,7 +6,9 @@ const { title } = require('process');
 
 const router = Router();
 
+//Get route that is accessed from public/assests/js/index.
 router.get('/', (req, res) => {
+	//Readfile to obtain the information form db/db.json When this is returned the index.js can generate a note list for the user.
 	fs.readFile('./db/db.json', 'utf-8', (err, data) => {
 		if (err) {
 			console.log(err);
@@ -18,18 +20,25 @@ router.get('/', (req, res) => {
 	});
 });
 
+//Post route that sends data from public/assets/js/index.
 router.post('/', (req, res) => {
+	//The datat from index.js will contain title of note and text.
 	const { title, text } = req.body;
+	//We grab all the current notes that have been generated in the past from the db/db.json
 	fs.readFile('./db/db.json', 'utf8', (err, data) => {
 		if (err) {
 			console.log(err);
 			res.status(400).send('Error with /notes post');
 		} else {
+			// Turn the datat from db.json into a JSON that we can manipulate
 			const currentData = JSON.parse(data);
+			//Add the newly sent over Text and Title to the JSON
 			currentData.push({ title, text });
+			//Run through the entire JSON and ensure all have an ID that can be  grabbed by the delete route.
 			currentData.forEach((note, position) => {
 				note.id = position;
 			});
+			//Updated db.json with all the changes we made after recieving the new note information from index.js
 			fs.writeFile(
 				'./db/db.json',
 				JSON.stringify(currentData),
@@ -46,12 +55,15 @@ router.post('/', (req, res) => {
 	res.send('Notes succesfully updated');
 });
 
+//Delete used to clear out notes after the trash can button is pressed.
 router.delete('/:id', (req, res) => {
+	//First we will grab the current notes that have been created from db/db.json
 	fs.readFile('./db/db.json', 'utf-8', (err, data) => {
 		if (err) {
 			console.log(err);
 			res.status(400).send('There was an error with /notes delete');
 		} else {
+			//We will need a new list that does not contain the note that index.js request to be deleted.
 			const currentList = JSON.parse(data);
 			const newList = currentList.filter((note) => {
 				return note.id != req.params.id;
